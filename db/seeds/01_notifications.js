306f020df1133189b910ea1b539a7366a77cf555
Katya Kuzmenchuk <kuzmenchukkatya@gmail.com>
2022-06-01 01:00:58 +0400

    🎲 Improve seed data

--- db/seeds/01_notifications.js
+++ db/seeds/01_notifications.js
@@ -6,10 +6,20 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('notifications').del()
   await knex('notifications').insert([
-    {type: 'share', payload: {test: 'test'}, user_id: 'beast~mailinator.com@marvel-x-men'},
-    {type: 'share', payload: {test: 'test'}, user_id: 'angel~mailinator.com@marvel-x-men'},
-    {type: 'entry-created', payload: {test: 'test'}, user_id: 'beast~mailinator.com@marvel-x-men'},
-    {type: 'entry-updated', payload: {test: 'test'}, user_id: 'angel~mailinator.com@marvel-x-men'},
-    {type: 'entry-created', payload: {test: 'test'}, user_id: 'beast~mailinator.com@marvel-x-men'},
+    {type: 'entry-created', payload: {entryTitle: '001', stepTitle: 'New', folderTitle: 'Reports', user: 'Henry McCoy'}, user_id: 'beast~mailinator.com@marvel-x-men', read: true},
+    {type: 'entry-created', payload: {entryTitle: '001', stepTitle: 'New', folderTitle: 'Reports', user: 'Henry McCoy'}, user_id: 'angel~mailinator.com@marvel-x-men', read: false},
+    {type: 'entry-created', payload: {entryTitle: '002', stepTitle: 'New', folderTitle: 'Reports', user: 'Henry McCoy'}, user_id: 'beast~mailinator.com@marvel-x-men', read: true},
+    {type: 'entry-created', payload: {entryTitle: '002', stepTitle: 'New', folderTitle: 'Reports', user: 'Henry McCoy'}, user_id: 'angel~mailinator.com@marvel-x-men', read: false},
+    {type: 'entry-created', payload: {entryTitle: '003', stepTitle: 'New', folderTitle: 'Reports', user: 'Henry McCoy'}, user_id: 'beast~mailinator.com@marvel-x-men', read: true},
+    {type: 'entry-created', payload: {entryTitle: '003', stepTitle: 'New', folderTitle: 'Reports', user: 'Henry McCoy'}, user_id: 'angel~mailinator.com@marvel-x-men', read: false},
+
+    {type: 'entry-moved', payload: {entryTitle: '002', fromStepTitle: 'New', toStepTitle: 'Open', folderTitle: 'Reports', user: 'Angel' +
+            ' Avening'}, user_id: 'angel~mailinator.com@marvel-x-men', read: false},
+    {type: 'entry-moved', payload: {entryTitle: '002', fromStepTitle: 'New', toStepTitle: 'Open', folderTitle: 'Reports', user: 'Angel' +
+            ' Avening'}, user_id: 'beast~mailinator.com@marvel-x-men', read: false},
+    {type: 'entry-moved', payload: {entryTitle: '003', fromStepTitle: 'New', toStepTitle: 'Open', folderTitle: 'Reports', user: 'Emma' +
+            ' Frost'}, user_id: 'angel~mailinator.com@marvel-x-men', read: false},
+    {type: 'entry-moved', payload: {entryTitle: '003', fromStepTitle: 'New', toStepTitle: 'Open', folderTitle: 'Reports', user: 'Emma' +
+            ' Frost'}, user_id: 'beast~mailinator.com@marvel-x-men', read: false},
   ]);
 };
