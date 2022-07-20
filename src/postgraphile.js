1b8a0cab91633d2e4ca31ad5b0927960fe00aacd
Pavel Titenkov <pavel@titenkov.com>
2022-07-20 23:05:10 +0300

    🧹 Clean up and headers unification

--- src/postgraphile.js
+++ src/postgraphile.js
@@ -24,37 +24,40 @@ module.exports = postgraphile(
 
         settings['role'] = 'anonymous';
 
-        if ('jwt' === AUTH_TYPE) {
-          // Auth0-integration
-          if (req.auth) {
-            settings['role'] = 'simple_user';
-            settings['request.user_id'] = req.auth[AUTH_USER_CLAIM];
-          }
+        // Verify required headers
+        if (!req.headers) {
+          throw new Error('Missing headers in request');
         }
+        if (!req.headers['x-api-key']) {
+          throw new Error(`Missing required header 'x-api-key'`);
+        }
+        if (!req.headers['x-user-id']) {
+          throw new Error(`Missing required header 'x-user-id'`);
+        }
+
+        // Fetch api-secret-key using public key
+        const apiPublicKey = req.headers['x-api-key']
+        const userId = req.headers['x-user-id']
+        const project = await fetchProjectByKey(apiPublicKey)
+
+        if (!project || !project.api_secret_key) {
+          throw new Error(`Failed to authenticate using '${apiPublicKey}' api key`);
+        }
+
+        const apiSecretKey = project.api_secret_key;
+        const projectId = project.id;
+
 
         if ('hmac' === AUTH_TYPE) {
           // HMAC-based authentication
           // Read more at https://notifir.github.io/docs/integration/authentication
-          if (!req.headers) {
-            throw new Error('Missing headers in request');
-          }
-          if (!req.headers['api-public-key'] || !req.headers['x-user-id'] || !req.headers['x-user-hmac']) {
-            throw new Error('Missing one of the required headers in request: [api-public-key, x-user-id or x-user-hmac]');
-          }
 
-          // Fetch api-secret-key using public key
-          const apiPublicKey = req.headers['api-public-key']
-          const project = await fetchProjectByKey(apiPublicKey);
-
-          if (!project || !project.api_secret_key) {
-            throw new Error(`Failed to authenticate '${apiPublicKey}'`);
+          if (!req.headers['x-user-hmac']) {
+            throw new Error(`Missing required header 'x-user-hmac'`);
           }
-
-          const apiSecretKey = project.api_secret_key;
-          const projectId = project.id;
 
           // Verify HMAC
-          const userId = req.headers['x-user-id'];
+
           const userHmac = req.headers['x-user-hmac'];
 
           const calculatedUserHmac = crypto
@@ -66,21 +69,12 @@ module.exports = postgraphile(
             console.warn(`HMAC verification failed: ${userHmac} !== ${calculatedUserHmac}`);
             throw new Error(`Failed to authenticate '${userId}': HMAC mismatch`);
           }
-
-          // Set DB-level roles and attributes
-          settings['role'] = ('system' === userId) ? 'system_user' : 'simple_user';
-          settings['request.user_id'] = userId;
-          settings['request.project_id'] = projectId;
         }
 
-        if ('plain' === AUTH_TYPE) {
-          // Plain header-based auth.
-          // Do not use in production!
-          if (req.headers && req.headers['authorization-key']) {
-            settings['role'] = ('system' === req.headers['authorization-key']) ? 'system_user' : 'simple_user';
-            settings['request.user_id'] = req.headers['authorization-key'];
-          }
-        }
+        // Set DB-level roles and attributes
+        settings['role'] = ('system' === userId) ? 'system_user' : 'simple_user';
+        settings['request.user_id'] = userId;
+        settings['request.project_id'] = projectId;
 
         return settings;
       },
@@ -98,8 +92,8 @@ module.exports = postgraphile(
           if (req.connectionParams['authorization-key']) {
             req.headers['authorization-key'] = req.connectionParams['authorization-key'];
           }
-          if (req.connectionParams['api-public-key']) {
-            req.headers['api-public-key'] = req.connectionParams['api-public-key'];
+          if (req.connectionParams['x-api-key']) {
+            req.headers['x-api-key'] = req.connectionParams['x-api-key'];
           }
           if (req.connectionParams['x-user-id']) {
             req.headers['x-user-id'] = req.connectionParams['x-user-id'];
