630589263922eaadf532a71a6c3e8b8115f375c5
Katya Kuzmenchuk <kuzmenchukkatya@gmail.com>
2022-07-28 11:33:47 +0400

    🎲 Improve seed data

--- db/seeds/02_templates.js
+++ db/seeds/02_templates.js
@@ -6,11 +6,11 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('templates').del()
   await knex('templates').insert([
-    {type: 'entry-created', locale: 'en-GB', content: 'The entry {entryTitle} in {stepTitle} was created in {folderTitle} by {user}.'},
-    {type: 'entry-created', locale: 'nb-NO', content: 'Oppføringen {entryTitle} i {stepTitle} ble opprettet i {folderTitle} av {user}.'},
-    {type: 'entry-created', locale: 'sv-SE', content: 'Posten {entryTitle} i {stepTitle} har skapats i {folderTitle} av {user}.'},
-    {type: 'entry-moved', locale: 'en-GB', content: 'The entry {entryTitle} in {folderTitle} was moved from step {fromStepTitle} to step {toStepTitle} by {user}.'},
-    {type: 'entry-moved', locale: 'nb-NO', content: 'Oppføringen {entryTitle} i {folderTitle} ble flyttet fra steg {fromStepTitle} til steg {toStepTitle} av {user}.'},
-    {type: 'entry-moved', locale: 'sv-SE', content: 'Posten {entryTitle} i {folderTitle} har flyttats från steg {fromStepTitle} till steg {toStepTitle} av {user}.'},
+    {type: 'entry-created', locale: 'en-GB', content: 'The entry {entry} in {step} was created in {folder} by {user}.'},
+    {type: 'entry-created', locale: 'nb-NO', content: 'Oppføringen {entry} i {step} ble opprettet i {folder} av {user}.'},
+    {type: 'entry-created', locale: 'sv-SE', content: 'Posten {entry} i {step} har skapats i {folder} av {user}.'},
+    {type: 'entry-moved', locale: 'en-GB', content: 'The entry {entry} in {folder} was moved from step {from_step} to step {to_step} by {user}.'},
+    {type: 'entry-moved', locale: 'nb-NO', content: 'Oppføringen {entry} i {folder} ble flyttet fra steg {from_step} til steg {to_step} av {user}.'},
+    {type: 'entry-moved', locale: 'sv-SE', content: 'Posten {entry} i {folder} har flyttats från steg {from_step} till steg {to_step} av {user}.'},
   ]);
 };
