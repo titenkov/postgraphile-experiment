3699abe0fd1ef442d18d4929aefa2a0075419c59
Pavel Titenkov <pavel@titenkov.com>
2022-08-01 18:58:03 +0300

    📦 Docker setup and publishing

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM node:lts-alpine AS packages-installation
+
+WORKDIR /usr/src/notifir/api
+
+COPY package.json .
+COPY ./src src
+COPY ./db db
+COPY knexfile.js .
+
+RUN yarn install --silent --non-interactive && yarn cache clean
+
+################################################################################
+
+FROM node:lts-alpine AS api-production-stage
+
+WORKDIR /notifir/api
+
+ENV ENV_NAME production
+ENV NODE_ENV production
+ENV NODE_CONFIG_ENV production
+
+COPY --from=packages-installation /usr/src/notifir/api/package.json .
+COPY --from=packages-installation /usr/src/notifir/api/node_modules ./node_modules
+COPY --from=packages-installation /usr/src/notifir/api/src ./src
+COPY --from=packages-installation /usr/src/notifir/api/db ./db
+COPY --from=packages-installation /usr/src/notifir/api/knexfile.js ./knexfile.js
+
+# Start
+CMD yarn start
+
+EXPOSE 80
--- knexfile.js
+++ knexfile.js
@@ -1,54 +1,39 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const { CLIENT, DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env
+const { CLIENT, DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 module.exports = {
-    development: {
-        client: CLIENT,
-        connection: {
-            database: DB_DATABASE,
-            user: DB_USER,
-            password: DB_PASSWORD,
-            host: DB_HOST,
-            port: DB_PORT,
-        },
-        migrations: {
-            directory: __dirname + '/db/migrations',
-        },
-        seeds: {
-            directory: __dirname + '/db/seeds',
-        },
+  development: {
+    client: CLIENT,
+    connection: {
+      database: DB_DATABASE,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      host: DB_HOST,
+      port: DB_PORT,
     },
+    migrations: {
+      directory: __dirname + "/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds",
+    },
+  },
 
-    staging: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password',
-        },
-        pool: {
-            min: 2,
-            max: 10,
-        },
-        migrations: {
-            tableName: 'knex_migrations',
-        },
+  production: {
+    client: CLIENT,
+    connection: {
+      database: DB_DATABASE,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      host: DB_HOST,
+      port: DB_PORT,
     },
-
-    production: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password',
-        },
-        pool: {
-            min: 2,
-            max: 10,
-        },
-        migrations: {
-            tableName: 'knex_migrations',
-        },
+    migrations: {
+      directory: __dirname + "/db/migrations",
     },
-}
+    seeds: {
+      directory: __dirname + "/db/seeds",
+    },
+  },
+};
